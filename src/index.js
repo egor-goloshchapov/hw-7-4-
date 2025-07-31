0157ce2878c7d065fe61dfef493c8447c38e681b
egor-goloshchapov <141495264+Egor-Goloshchapov@users.noreply.github.com>
2025-07-31 23:10:33 +0300

    did hw-7

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import { products } from './data';
-import template from './template.hbs';
+import { products } from '../data';
 
 const container = document.getElementById('app');
-container.innerHTML = template({ products });
+
+const ul = document.createElement('ul');
+
+products.forEach(product => {
+  const li = document.createElement('li');
+
+  const name = document.createElement('h2');
+  name.textContent = product.name;
+
+  const description = document.createElement('p');
+  description.textContent = product.description;
+
+  const price = document.createElement('p');
+  price.innerHTML = `<strong>Price:</strong> $${product.price}`;
+
+  li.append(name, description, price);
+  ul.appendChild(li);
+});
+
+container.appendChild(ul);
+
+
